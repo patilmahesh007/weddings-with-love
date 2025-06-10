7684ba3e32b33d77cc5e20707384178c1c252249
patilmahesh007 <patilmahesh4007>
2025-06-10 17:28:48 +0530

    removed some thinsg

--- components/Header.tsx
+++ components/Header.tsx
@@ -97,7 +97,7 @@ export default function Header() {
           <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <Link href="/" className="-m-1.5 p-1.5">
-                <span className="font-playfair text-2xl font-bold">HOTC</span>
+                <span className="font-playfair text-2xl font-bold">WWL</span>
               </Link>
               <Button
                 variant="ghost"
